2871929cd12478a03e3f45c20ce3831db617f7fe
robbins <robbins@uk.ibm.com>
2017-06-15 15:28:36 +0100

    Ensure JsonException does not muck up the data

--- web/src/com/ibm/javametrics/HttpDataAggregator.java
+++ web/src/com/ibm/javametrics/HttpDataAggregator.java
@@ -19,6 +19,7 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map.Entry;
 
+import javax.json.JsonException;
 import javax.json.JsonObject;
 
 public class HttpDataAggregator {
@@ -27,9 +28,9 @@ public class HttpDataAggregator {
 	long longest;
 	long time;
 	String url;
-	
+
 	private HashMap<String, HttpUrlData> responseTimes = new HashMap<String, HttpUrlData>();
-	
+
 	public HttpDataAggregator() {
 		clear();
 	}
@@ -45,7 +46,7 @@ public class HttpDataAggregator {
 	HttpDataAggregator getCurrent() {
 		return new HttpDataAggregator(total, average, longest, time, url);
 	}
-	
+
 	void clear() {
 		total = 0;
 		average = 0;
@@ -54,31 +55,39 @@ public class HttpDataAggregator {
 		url = "";
 	}
 
-	public void aggregate(JsonObject jsonObject) {
-		
-		if (total == 0) {
-			time = jsonObject.getJsonNumber("time").longValue();
-		}
-		
-		total += 1;
+	public void aggregate(JsonObject jsonObject) throws JsonException {
 
+		// Json processing
+		// Extract the fields from the Json up front to avoid exceptions
+		// creating half baked request data 
+		long requestTime = jsonObject.getJsonNumber("time").longValue();
 		long requestDuration = jsonObject.getJsonNumber("duration").longValue();
 		String requestUrl = jsonObject.getString("url", "");
+		// Emd of Json processing
+
+		if (total == 0) {
+			time = requestTime;
+		}
+
+		total += 1;
+
 		if (requestDuration > longest || total == 1) {
 			longest = requestDuration;
 			url = requestUrl;
-		}	
-		average = ((average * (total-1)) + requestDuration)/total;	
-		
+		}
+		average = ((average * (total - 1)) + requestDuration) / total;
+
 		HttpUrlData urlData = responseTimes.get(requestUrl);
 		if (urlData == null) {
 			urlData = new HttpUrlData();
 		}
+
 		urlData.hits += 1;
-		urlData.averageResponseTime = ((urlData.averageResponseTime * (urlData.hits-1)) + requestDuration)/urlData.hits;
+		urlData.averageResponseTime = ((urlData.averageResponseTime * (urlData.hits - 1)) + requestDuration)
+				/ urlData.hits;
 		responseTimes.put(requestUrl, urlData);
 	}
-	
+
 	String toJsonString() {
 		StringBuilder sb = new StringBuilder("{\"topic\":\"http\",\"payload\":{\"time\":");
 		sb.append(time);
@@ -93,11 +102,11 @@ public class HttpDataAggregator {
 		sb.append("\"}}");
 		return sb.toString();
 	}
-	
+
 	private class HttpUrlData {
 		int hits;
 		long averageResponseTime;
-		
+
 		public HttpUrlData() {
 			hits = 0;
 			averageResponseTime = 0;
@@ -106,22 +115,22 @@ public class HttpDataAggregator {
 
 	public String urlDatatoJsonString() {
 		StringBuilder sb = new StringBuilder("{\"topic\":\"httpURLs\",\"payload\":[");
-		
-	    Iterator<Entry<String, HttpUrlData>> it = responseTimes.entrySet().iterator();
-        boolean first = true;
-	    while (it.hasNext()) {
-	        Entry<String, HttpUrlData> pair = it.next();
+
+		Iterator<Entry<String, HttpUrlData>> it = responseTimes.entrySet().iterator();
+		boolean first = true;
+		while (it.hasNext()) {
+			Entry<String, HttpUrlData> pair = it.next();
 			if (!first) {
-	        	sb.append(',');
-	        }
+				sb.append(',');
+			}
 			first = false;
-	        sb.append("{\"url\":\"");
-	        sb.append(pair.getKey());
-	        sb.append("\",\"averageResponseTime\":");
-	        sb.append(pair.getValue().averageResponseTime);
-	        sb.append('}');
-	    }
-	    
+			sb.append("{\"url\":\"");
+			sb.append(pair.getKey());
+			sb.append("\",\"averageResponseTime\":");
+			sb.append(pair.getValue().averageResponseTime);
+			sb.append('}');
+		}
+
 		sb.append("]}");
 		return sb.toString();
 	}
