326d173366eb64a660cfc6efd44d43086656bca9
stalleyj <julie_stalley@uk.ibm.com>
2017-07-26 09:35:41 +0100

    Spring config

---
+++ dashboard/src/main/java/com/ibm/javametrics/web/WebMVCConfig.java
@@ -0,0 +1,15 @@
+package hello;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class WebMVCConfig extends WebMvcConfigurerAdapter {
+
+@Override
+public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/javametrics-dash").setViewName("./javametrics/index");
+
+   }
+} 
